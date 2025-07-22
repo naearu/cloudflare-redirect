2462dc8c0fd4b1c001fe18991bbcb3928e3aa86a
KwangJin Go <naearu2@gmail.com>
2025-07-22 18:16:38 +0900

    수정

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
+export interface Env {
+  REDIRECT_TO: string;
+}
+
 export default {
 	async fetch(request): Promise<Response> {
-		const destinationURL = `${env.redirect_to}`;
+		const destinationURL = `${env.REDIRECT_TO}`;
 		const statusCode = 301;
 		return Response.redirect(destinationURL, statusCode);
 	},
